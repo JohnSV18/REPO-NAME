293f09ff28b56936959f981a63b739daa94a2b02
John Marcos <johnsaguay@gmail.com>
2021-09-15 08:56:47 -0700

    Implemented responding to JSON

--- test/test-pets.js
+++ test/test-pets.js
@@ -102,6 +102,18 @@ describe('Pets', ()  => {
       });
     });
   });
+  // Testing API endpoints
+  it('should list ALL pets on /pets GET', function(done) {
+    chai.request(server)
+        .get('/')
+        .set('content-type', 'application/json')
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          done();
+        });
+  });
 
   // TEST DELETE
   it('should delete a SINGLE pet on /pets/<id> DELETE', (done) => {
